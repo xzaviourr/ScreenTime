5790aed6571376fac5bdd1a5574a4f02113c81ce
Anshul Sanghi <xzaviourr@gmail.com>
2022-11-19 02:44:25 +0530

    added static ui

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtCore
 
-from modules.pyplot_designer import WidgetPlot
+from modules.pyplot_designer import PieChartsWidgetPlot, BarChartsWidgetPlot, LineChartsWidgetPlot
 from ui.main_ui import Ui_MainWindow
 
 class window(Ui_MainWindow, QMainWindow):
@@ -10,10 +11,87 @@ class window(Ui_MainWindow, QMainWindow):
         super().__init__(parent)
         self.setupUi(self)
 
-        self.stats_grapher = WidgetPlot()
-        self.StatsGraph_QHBoxLaybout.addWidget(self.stats_grapher)
-        self.stats_grapher.plot_stats_pie_chart()
+        self.pie_chart_stats_grapher = PieChartsWidgetPlot()
+        self.StatsGraph_QHBoxLaybout.addWidget(self.pie_chart_stats_grapher)
 
+        self.bar_chart_stats_grapher = BarChartsWidgetPlot()
+        self.BarChartStats_QHBoxLayout.addWidget(self.bar_chart_stats_grapher)
+
+        self.line_chart_stats_grapher = LineChartsWidgetPlot()
+        self.HBarChartStats_QHBoxLayout.addWidget(self.line_chart_stats_grapher)
+
+        self.plot_category_stats()
+        self.plot_daily_stats()
+        self.change_live_stats()
+        self.plot_application_stats()
+
+    def plot_category_stats(self):
+        """
+        Plots user category stats pie charts
+        """
+        daily_stats = {
+            "values": [10, 20, 15],
+            "labels": ["Entertainment", "Work", "College"]
+        }
+
+        weekly_stats = {
+            "values": [10, 30, 5, 20],
+            "labels": ["Shopping", "Work", "Entertainment", "College"]
+        }
+
+        monthly_stats = {
+            "values": [80, 10, 20, 40],
+            "labels": ["Entertainment", "Coding", "Work", "College"]
+        }
+
+        self.pie_chart_stats_grapher.plot_stats_pie_chart(
+            daily_status = daily_stats,
+            weekly_stats = weekly_stats,
+            monthly_stats = monthly_stats
+        )
+
+    def plot_daily_stats(self):
+        """
+        Plots user stats for today
+        """
+        today_stats = {
+            "values": [10, 20, 15, 40, 15, 20],
+            "labels": ["Teams", "Moodle", "Chrome", "VS Code", "Counter Strike", "Notepad"]
+        }
+        self.bar_chart_stats_grapher.plot_stats_bar_chart(
+            today_stats = today_stats
+        )
+
+    def change_live_stats(self):
+        """
+        Updates live stats of screen time
+        """
+        today = "01h 25m"
+        week = "08h 20m"
+        month = "24h 15m"
+
+        self.TimeToday_Label.setText(today)
+        self.TimeWeek_Label.setText(week)
+        self.TimeMonth_Label.setText(month)
+
+        self.TimeToday_Label.setStyleSheet("font-size: 72pt; font-weight: bold; text-align: right")
+        self.TimeWeek_Label.setStyleSheet("font-size: 48pt; font-weight: bold; text-align: right")
+        self.TimeMonth_Label.setStyleSheet("font-size: 36pt; font-weight: bold; text-align: right")
+        
+        self.TimeToday_Label.setAlignment(QtCore.Qt.AlignRight)
+        self.TimeWeek_Label.setAlignment(QtCore.Qt.AlignRight)
+        self.TimeMonth_Label.setAlignment(QtCore.Qt.AlignRight)
+
+    def plot_application_stats(self):
+        """
+        Plots weekly and monthly line chart stats
+        """
+        weekly_stats = [10, 20, 5, 30, 40, 15, 20]
+        monthly_stats = [100, 120, 200, 130, 80, 40, 50, 27, 48, 12, 23, 235, 123, 23, 235, 23, 234, 23,4 ,23, 234]
+        self.line_chart_stats_grapher.plot_stats_line_chart(
+            weekly_stats = weekly_stats,
+            monthly_stats = monthly_stats
+        )
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- modules/pyplot_designer.py
+++ modules/pyplot_designer.py
@@ -6,7 +6,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib as plt
 
-class PlotCanvas(FigureCanvas):
+class PieChartCanvas(FigureCanvas):
     """
     Instance that plots all graphs on the UI
     """
@@ -23,7 +23,15 @@ class PlotCanvas(FigureCanvas):
             QSizePolicy.Expanding
         )
 
-    def plot_stats_pie_chart(self, daily_status, weekly_stats, monthly_stats):
+    def plot_stats_pie_chart(self, daily_status:dict, weekly_stats:dict, monthly_stats:dict):
+        """
+        Plots pie charts for daily, weekly and monthly stats
+
+        Args :
+            daily_stats : dict[values, labels] : daily stats
+            weekly_stats : dict[values, labels] : weekly stats
+            monthly_stats : dict[values, labels] : monthly stats
+        """
         def get_sector(x, values):
             abs_value = int(x / 100.*np.sum(values))
             return "{:.1f}%\n({:d} mins)".format(x, abs_value)
@@ -34,9 +42,11 @@ class PlotCanvas(FigureCanvas):
         for idx, chart in enumerate(['Daily Stats', "Weekly Stats", "Monthly Stats"]):
             ax = self.figure.add_subplot(1, 3, idx+1)
             
-            values = [10, 20, 15]
-            labels = ["Moodle", "Chrome", "Teams"]
-            explode = (0.1, 0.0, 0.2)
+            values = list(idx_mapping[idx]['values'])
+            labels = list(idx_mapping[idx]['labels'])
+            highest_time_consuming_index = values.index(max(values))
+            explode = [0 for x in range(len(values))]
+            explode[highest_time_consuming_index] = 0.2
 
             w, t, at = ax.pie(
                 x = values,
@@ -45,26 +55,136 @@ class PlotCanvas(FigureCanvas):
                 labels = labels,
                 shadow = True,
                 startangle = 90, 
-                radius = 1.3,
+                radius = 1.35,
                 wedgeprops = {'linewidth': 1, 'edgecolor': "black"},
-                textprops = dict(color = "black", size = 8)
+                textprops = dict(color = "black", size = 10)
             )
 
-            # if idx == 2:
-            #     ax.legend(w, labels, title = "Stats", loc = "center right")
-            ax.set_title(chart)
+            ax.set_title(chart, pad = 15)
 
-        self.figure.tight_layout(pad=10)
+        self.figure.tight_layout(pad=8)
         self.draw()
 
 
-
-class WidgetPlot(QWidget):
+class PieChartsWidgetPlot(QWidget):
     def __init__(self, *args, **kwargs):
         QWidget.__init__(self, *args, **kwargs)
         self.setLayout(QVBoxLayout())
-        self.canvas = PlotCanvas(self)
+        self.canvas = PieChartCanvas(self)
         self.layout().addWidget(self.canvas)
 
-    def plot_stats_pie_chart(self):
-        self.canvas.plot_stats_pie_chart()
+    def plot_stats_pie_chart(self, daily_status:dict, weekly_stats:dict, monthly_stats:dict):
+        self.canvas.plot_stats_pie_chart(daily_status, weekly_stats, monthly_stats)
+
+
+class BarChartCanvas(FigureCanvas):
+    """
+    Instance that plots all graphs on the UI
+    """
+    def __init__(self, parent=None, height=10, width=20, dpi=100):
+        self.figure = Figure(figsize=(width, height), dpi=dpi)
+
+        FigureCanvas.__init__(self, self.figure)
+        self.setParent(parent)
+
+        # Set width and size to be expanding to cover up the widget
+        FigureCanvas.setSizePolicy(
+            self,
+            QSizePolicy.Expanding,
+            QSizePolicy.Expanding
+        )
+
+    def plot_stats_bar_chart(self, today_stats:dict):
+        """
+        Plots pie charts for daily, weekly and monthly stats
+
+        Args :
+            today_stats : dict[values, labels] : today's stats
+        """
+        self.figure.clear(True)
+        ax = self.figure.add_subplot(1, 1, 1)
+        
+        values = list(today_stats['values'])
+        labels = list(today_stats['labels'])
+        
+        colors = []
+        for i in range(0, len(labels)+1):
+            colors.append(np.random.rand(1, 3))
+
+        ax.bar(
+            x = labels,
+            height = values,
+            width = 0.8,
+            color = colors
+        )
+
+        ax.set_xlabel("Application")
+        ax.set_ylabel("Minutes")
+        ax.set_title("Time Spent Today", pad = 15)
+
+        self.figure.tight_layout(pad=8)
+        self.draw()
+
+
+class BarChartsWidgetPlot(QWidget):
+    def __init__(self, *args, **kwargs):
+        QWidget.__init__(self, *args, **kwargs)
+        self.setLayout(QVBoxLayout())
+        self.canvas = BarChartCanvas(self)
+        self.layout().addWidget(self.canvas)
+
+    def plot_stats_bar_chart(self, today_stats:dict):
+        self.canvas.plot_stats_bar_chart(today_stats)
+
+
+class LineChartCanvas(FigureCanvas):
+    """
+    Instance that plots all graphs on the UI
+    """
+    def __init__(self, parent=None, height=10, width=20, dpi=100):
+        self.figure = Figure(figsize=(width, height), dpi=dpi)
+
+        FigureCanvas.__init__(self, self.figure)
+        self.setParent(parent)
+
+        # Set width and size to be expanding to cover up the widget
+        FigureCanvas.setSizePolicy(
+            self,
+            QSizePolicy.Expanding,
+            QSizePolicy.Expanding
+        )
+
+    def plot_stats_line_chart(self, weekly_stats:list, monthly_stats:list):
+        """
+        Plots line chart for weekly and monthly stats
+
+        Args :
+            weekly_stats : [values] : weekly stats
+            monthly_stats : [values] : monthly stats
+        """
+        self.figure.clear(True)
+        
+        idx_mapping = {0: weekly_stats, 1:monthly_stats}
+        for idx, grp in enumerate(["Last Week", "Last Month"]):
+            ax = self.figure.add_subplot(2, 1, idx+1)
+            values = list(idx_mapping[idx])
+            indices = [i+1 for i in range(len(values))]
+            ax.plot(indices, values, marker='o')
+            ax.fill_between(indices, values, alpha=0.3)
+            ax.set_xlabel("Day")
+            ax.set_ylabel("Time")
+            ax.set_title(grp, pad = 8)
+
+        self.figure.tight_layout(pad=15)
+        self.draw()
+
+
+class LineChartsWidgetPlot(QWidget):
+    def __init__(self, *args, **kwargs):
+        QWidget.__init__(self, *args, **kwargs)
+        self.setLayout(QVBoxLayout())
+        self.canvas = LineChartCanvas(self)
+        self.layout().addWidget(self.canvas)
+
+    def plot_stats_line_chart(self, weekly_stats:list, monthly_stats:list):
+        self.canvas.plot_stats_line_chart(weekly_stats, monthly_stats)
